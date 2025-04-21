7943cb6544dad92f63fb8d7d429a62770f5b76bf
Akhmed <ahmedzadaev2@gmail.com>
2025-04-22 02:19:24 +0300

    fibo

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let numNumber = +num;
+    if (numNumber < 0) {
+        return "OOPS";
+    }
+    else if (numNumber == 0){
+        return 0;
+    }
+    else {
+        let firstFibo = 1;
+        let secondFibo = 1;
+        for (let i = 3; i <= numNumber; i++) {
+            let currentFibo = firstFibo + secondFibo;
+            firstFibo = secondFibo;
+            secondFibo = currentFibo;
+        }
+        return secondFibo;
+    } 
 };
 
 // еще две задачи
