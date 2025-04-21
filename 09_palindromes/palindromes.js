f917eb3c35c694659279f4bf98ac97091140929c
Akhmed <ahmedzadaev2@gmail.com>
2025-04-22 01:35:43 +0300

    pallindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,5 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+return str.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s/g, '') == str.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s/g, '').split('').reverse().join('');
 };
 
 // уже почти пять утра
